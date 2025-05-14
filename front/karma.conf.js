efc80713832f616533be31a728527d228b1e3f92
miere221 <william.miere@etu.unicaen.fr>
2025-05-14 20:17:10 +0200

    fix lcov

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -29,7 +29,9 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'cobertura' },
+        { type: 'lcovonly' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
